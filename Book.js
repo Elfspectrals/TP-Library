e0e94b4cfa309c40a4b9c60c4dc6309f804be99f
Jerome <jerome.neupert@epitech.eu>
2025-02-26 12:15:09 +0100

    step 1

---
+++ Book.js
@@ -0,0 +1,9 @@
+export default class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+    getDetails(){
+        return `Titre : ${this.title} / Auteur : ${this.author}`;
+    }
+}
---
+++ app.js
@@ -0,0 +1,4 @@
+import Book from './Book.js';
+
+const LOTR = new Book('Le Seigneur des Anneaux', 'J.R.R. Tolkien');
+console.log(LOTR.getDetails());
